83db4a4c3a2c25285d17059ef59434f3affd3a38
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2024-11-01 01:52:45 +0700

    customer change

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/menu', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'menu.html'));
 });
 
 module.exports = router;
